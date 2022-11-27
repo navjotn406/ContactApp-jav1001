6062adc0c3803ae951729e5d0c7efada577a4309
navjotn406 <113278709+navjotn406@users.noreply.github.com>
2022-11-27 12:52:37 -0500

    Initial commit (layout)

---
+++ app/src/main/java/com/example/contactapp_jav1001/ContactModel.kt
@@ -0,0 +1,12 @@
+package com.example.contactapp_jav1001
+
+class ContactModel {
+
+        var id: Int = 0
+        var first_name: String = ""
+        var last_name: String = ""
+        var contact_number: String = ""
+        var email: String = ""
+        var address: String = ""
+        var byteArray: ByteArray = ByteArray(1000)
+    }
